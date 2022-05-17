cef187047a858e060dec9ae1479b5885ea36cc61
Pawel Nowosielski <pawel.nowosielski@eigen.co>
2022-05-17 18:54:11 +0200

    add new print line

--- src/com/futurecollars/Main.java
+++ src/com/futurecollars/Main.java
@@ -3,5 +3,6 @@ package com.futurecollars;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! I'm on GitHub!");
+        System.out.println("New line added");
     }
 }
